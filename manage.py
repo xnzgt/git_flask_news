bcdf3348835fea3698820994b0ba7d4788116a56
谢年智 <1248683517@qq.com>
2019-06-06 21:39:51 +0800

    配置Session并设置四项参数

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 # 4:设置csrf验证
 from flask_wtf import CSRFProtect
+# 5:将session保存到Session中
+from flask_session import Session
+from redis import StrictRedis
 
 app = Flask(__name__)
 
@@ -16,13 +19,26 @@ class Config(object):
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
 
+    # 配置flask_session四项参数
+    # 指定session存储方式
+    SESSION_TYPE = "redis"
+    # 指定储存session的储存对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 设置session签名
+    SESSION_USR_SIGNER = True
+    # 设置session不永久保存
+    SESSION_PERMANENT = False
+    # 设置session保存时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 app.config.from_object(Config)
 # 初始化数据库对象
 db = SQLAlchemy(app)
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 # 开启csrf
-app.config.from_object(Config)
 CSRFProtect(app)
+# 初始化Session
+Session(app)
 
 @app.route('/')
 def index():
