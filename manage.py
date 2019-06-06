0dafb68875c44b191a66f149f378e9526e26ec9b
谢年智 <1248683517@qq.com>
2019-06-06 21:27:59 +0800

    集成配置类配置数据库

--- manage.py
+++ manage.py
@@ -1,6 +1,19 @@
 from flask import Flask
+# 2:配置数据库
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# 1:集成配置类
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1/git_flask_news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
