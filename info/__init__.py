ce6745fe5b37dc128ed0a830385e35c812b02b3b
谢年智 <1248683517@qq.com>
2019-06-06 22:25:41 +0800

    定义创建app方法传入环境字典参数

--- info/__init__.py
+++ info/__init__.py
@@ -4,15 +4,17 @@ from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
-from config import Config
+from config import config
 
 app = Flask(__name__)
-app.config.from_object(Config)
-# 初始化数据库对象
-db = SQLAlchemy(app)
+def create_app(config_name):
+    app.config.from_object(config[config_name])
+    # 初始化数据库对象
+    db = SQLAlchemy(app)
 
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-# 开启csrf
-CSRFProtect(app)
-# 初始化Session
-Session(app)
+    redis_store = redis.StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    # 开启csrf
+    CSRFProtect(app)
+    # 初始化Session
+    Session(app)
+    return app
