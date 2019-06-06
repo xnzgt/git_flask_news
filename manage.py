49f3559d43c3c8d1d872d83415abc1cea51eef6c
谢年智 <1248683517@qq.com>
2019-06-06 21:32:23 +0800

    配置redis

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask import Flask
 # 2:配置数据库
 from flask_sqlalchemy import SQLAlchemy
+# 3:集成redis
+import redis
 
 app = Flask(__name__)
 
@@ -9,14 +11,18 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1/git_flask_news"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 app.config.from_object(Config)
-
+# 初始化数据库对象
 db = SQLAlchemy(app)
-
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
 @app.route('/')
 def index():
+    # 测试redis
+
     return "helloWorld"
 
 if __name__ == '__main__':
