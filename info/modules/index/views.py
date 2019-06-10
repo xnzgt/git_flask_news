e628b0e226b75839becd4c9c3f834271ce2afc08
谢年智 <1248683517@qq.com>
2019-06-10 15:05:46 +0800

    点击排行后端代码实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,13 +1,29 @@
 from flask import render_template, current_app, session, jsonify
 
-from info import redis_store
-from info.models import User
+from info import redis_store, constants
+from info.models import User, News
 from info.modules.index import index_blu
 from info.utils.response_code import RET
 
 
 @index_blu.route('/')
 def index():
+    # 排行榜设计
+    """
+    从数据库中查出排行前六的列表
+    将列表对象转化为字典
+    发送给前端展示
+    :return:
+    """
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据库查询排行失败")
+    news_hot = [news.to_basic_dict for news in news_list]
+    print("新闻排行:%s" % news_hot)
+
+
     # 设置首页右上角用户名显示
     """
     使用session中的user_id来判断用户是否登录
@@ -25,7 +41,8 @@ def index():
     # 将列表中的数据库对象转换为字典
     user = user.to_dict() if user else None
     data = {
-        "user_info":user
+        "user_info":user,
+        "news_hot":news_hot
     }
 
     return render_template("news/index.html",data = data)
