4feb588f2e9d7ea6303afd1bab75f9b5090e0852
谢年智 <1248683517@qq.com>
2019-06-10 11:18:51 +0800

    用户注册前端代码测试

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -22,7 +22,6 @@ def register():
     向前端返回响应
     :return:
     """
-    return jsonify(errno=RET.OK, errmsg="短信发送成功")
     param_dict = request.json
     mobile = param_dict.get("mobile")
     smscode = param_dict.get("smscode")
