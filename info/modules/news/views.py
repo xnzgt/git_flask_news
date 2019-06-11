2511927efd2ecd05f2e4312a896cbdfaf69da790
谢年智 <1248683517@qq.com>
2019-06-11 20:51:44 +0800

    抽取模板

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -6,4 +6,7 @@ from info.modules.news import news_blu
 
 @news_blu.route("/<int:news_id>")
 def news_detail(news_id):
-    return render_template("news/detail.html")
+    data = {
+        "news":""
+    }
+    return render_template("news/detail.html", data = data)
